e857dff74a7b0128f1ffafeaecba0a9e8aeb310d
Justin Love <git@JustinLove.name>
2014-02-27 06:33:22 -0600

    identify fabrication units

--- ui/mods/selected_commander/selected_commander.js
+++ ui/mods/selected_commander/selected_commander.js
@@ -1,6 +1,12 @@
 (function() {
+  model.selectedCommanderClasses = function(type) {
+    var path = type.replace("pa/units", "").replace(".json", "")
+    var fab = type.match('fabrication') ? ' fabrication' : ''
+    return path + fab
+  }
   // either knockout or the dom remove the rest of the illegal characters
-  var hack = ',css:type.replace("pa/units", "").replace(".json", "")'
+  //var hack = ',css:type.replace("pa/units", "").replace(".json", "")'
+  var hack = ',css:$parent.selectedCommanderClasses(type)'
 
   var bind = $('.div_unit_selection').attr('data-bind')
   $('.div_unit_selection').attr('data-bind', bind + hack)
