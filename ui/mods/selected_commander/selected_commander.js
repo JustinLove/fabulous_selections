85e992a12401babf6bf4f1638a5c89e25705fba7
Justin Love <git@JustinLove.name>
2014-02-28 21:00:51 -0600

    update for gamma

--- ui/mods/selected_commander/selected_commander.js
+++ ui/mods/selected_commander/selected_commander.js
@@ -1,13 +1,13 @@
 (function() {
   model.selectedCommanderClasses = function(type) {
+    // either knockout or the dom remove the rest of the illegal characters
     var path = type.replace("pa/units", "").replace(".json", "")
     var fab = type.match('fabrication') ? ' fabrication' : ''
     return path + fab
   }
-  // either knockout or the dom remove the rest of the illegal characters
-  //var hack = ',css:type.replace("pa/units", "").replace(".json", "")'
-  var hack = ',css:$parent.selectedCommanderClasses(type)'
+
+  var hack = 'css:$parent.selectedCommanderClasses(type)'
 
   var bind = $('.div_unit_selection').attr('data-bind')
-  $('.div_unit_selection').attr('data-bind', bind + hack)
+  $('.div_unit_selection').attr('data-bind', [bind, hack].join(','))
 })()
