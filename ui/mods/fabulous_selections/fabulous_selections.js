066dc48d64ad09b75a7e261471167ca5b09c2626
Justin Love <git@JustinLove.name>
2014-03-01 19:45:02 -0600

    rename from selected_commander to fabulous_selections

--- ui/mods/fabulous_selections/fabulous_selections.js
+++ ui/mods/fabulous_selections/fabulous_selections.js
@@ -1,12 +1,12 @@
 (function() {
-  model.selectedCommanderClasses = function(type) {
+  model.fabulousSelectionClasses = function(type) {
     // either knockout or the dom remove the rest of the illegal characters
     var path = type.replace("pa/units", "").replace(".json", "")
     var fab = type.match('fabrication') ? ' fabrication' : ''
     return path + fab
   }
 
-  var hack = 'css:$parent.selectedCommanderClasses(type)'
+  var hack = 'css:$parent.fabulousSelectionClasses(type)'
 
   var bind = $('.div_unit_selection').attr('data-bind')
   $('.div_unit_selection').attr('data-bind', [bind, hack].join(','))
